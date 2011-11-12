b6b0ba84f5e0cb621ec907f65158f8d5917aef16
James Padolsey <jamespadolsey@gmail.com>
2011-11-12 23:14:20 +0000

    Log

--- app/server.js
+++ app/server.js
@@ -3,3 +3,4 @@ var http = require('http'),
 	jssrc = require('./jsapi/jsapi');
 
 http.createServer(jssrc.server).listen(8001);
+console.log('Server running on 8001. Try: http://127.0.0.1:8001/jquery/css');
